015a916d20ec6b7f98b9a09f8f7960a414993179
Rafał Jaworski <rjawor@amu.edu.pl>
2017-03-07 09:13:48 +0100

    lemmagen sentence lemmatizer

---
+++ mgiza-aligner/LemmaGenSentenceLemmatizer/LemmaGenSentenceLemmatizer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemmaGenSentenceLemmatizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                SentenceLemmatizer lemmatizer = new SentenceLemmatizer(args[0]);
+                string line = Console.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    Console.WriteLine(lemmatizer.lemmatizeSentence(line));
+                    line = Console.ReadLine();
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong number of parameters. Please specify the language code as the only parameter");
+            }
+
+        }
+    }
+}
---
+++ mgiza-aligner/LemmaGenSentenceLemmatizer/LemmaGenSentenceLemmatizer/SentenceLemmatizer.cs
@@ -0,0 +1,81 @@
+﻿using LemmaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemmaGenSentenceLemmatizer
+{
+    class SentenceLemmatizer
+    {
+        private char[] wordInnerSeparator = { '-' };
+
+        private ILemmatizer lemmatizer;
+
+        public SentenceLemmatizer(string languageCode)
+        {
+            switch (languageCode)
+            {
+                case "pl":
+                    lemmatizer = new LemmatizerPrebuiltCompact(LanguagePrebuilt.Polish);
+                    break;
+                case "en":
+                    lemmatizer = new LemmatizerPrebuiltCompact(LanguagePrebuilt.English);
+                    break;
+                case "hr":
+                    lemmatizer = new LemmatizerPrebuiltCompact(LanguagePrebuilt.Serbian);
+                    break;
+                default:
+                    throw new ArgumentException("Unknown language code: " + languageCode);
+            }
+        }
+
+        public string lemmatizeSentence(string sentence)
+        {
+            string[] tokens = sentence.Split(null);
+
+            string result = "";
+            foreach (string token in tokens)
+            {
+                result += lemmatizeWord(token) + " ";
+            }
+
+            return result.Trim();
+        }
+
+        private string lemmatizeWord(string word)
+        {
+            string result = "";
+            string[] parts = word.Split(wordInnerSeparator);
+            if (parts.Length == 2)
+            {
+                string firstPart = parts[0];
+                if (!parts[0].EndsWith("o"))
+                {
+                    firstPart = lemmatizer.Lemmatize(firstPart);
+                }
+                string secondPart = lemmatizer.Lemmatize(parts[1]);
+                result = firstPart + "-" + secondPart;
+            }
+            else
+            {
+                result = lemmatizer.Lemmatize(word);
+            }
+
+
+
+            if (result == "" || result.Contains(" "))
+            {
+                return word;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+
+
+    }
+}
